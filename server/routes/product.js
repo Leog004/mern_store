2dc156f8b478aa17abf1fc40599726bae1c58f5c
LEO\leog4 <leog4za@gmail.com>
2022-03-05 22:02:13 -0600

    crud operations to products routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -6,19 +6,48 @@ const Product = require("../models/Product");
 router.get('/', async (req, res) => {
 
     const query = req.query.new;
+    const qCategory = req.query.category;
 
     try {
-        const prducts = query 
-        ? await Product.find().sort({_id: 1}).limit(5)
-        : Product.find();
 
-        res.status(200).json(prducts);
+        let products;
+
+        if(query){
+            products = await Product.find().sort({createdAt: -1}).limit(5);
+        }else if (qCategory){
+            products = await Product.find({
+                categories: {
+                    $in: [qCategory],
+                }
+            })
+        }else{
+            products = await Product.find();
+        }
+        
+        res.status(200).json(products);
+
     }
 
     catch(err) {
         res.status(500).json("Something has gone wrong");
     }
 });
+
+
+// GET PRODUCT BY ID
+router.get('/:id', async (req, res) => {
+
+    try {
+        const product = await Product.findById(req.params.id);
+
+        res.status(200).json(product);
+    }
+
+    catch(err) {
+        res.status(500).json("Something has gone wrong");
+    }
+});
+
 
 
 // CREATE A PRODUCT
@@ -41,6 +70,41 @@ router.post('/', verifyTokenAndAdmin, async (req, res) => {
 
 
 })
+
+
+
+// UPDATE PRODUCT
+router.post('/:id', verifyTokenAndAdmin, async (req, res) => {
+
+    try {
+
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id,{
+            $set: req.body,
+        }, {new: true});
+
+        res.status(200).json(updatedProduct);
+
+    } catch (err) {
+
+        return res.status(500).json(err);
+    }
+
+})
+
+
+// DELETE
+router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
+
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleeted");
+    }
+
+    catch(err) {
+        res.status(500).json("Something has gone wrong");
+    }
+});
+
 
 
 
